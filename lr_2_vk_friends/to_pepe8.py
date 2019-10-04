d6dd2dae77c63513570a869d2b0c026a7e7c0d16
123 <enbsfn@gmail.com>
2019-10-04 13:53:33 +0300

    123

---
+++ lr_2_vk_friends/to_pepe8.py
@@ -0,0 +1,13 @@
+import json
+import vk
+import requests
+import time
+import networkx as nx
+import matplotlib.pyplot as plt
+from IPython.display import Image
+from IPython.core.display import HTML
+from PIL import Image
+from io import BytesIO
+from matplotlib.pyplot import imshowy
+
+
